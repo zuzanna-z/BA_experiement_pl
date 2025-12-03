f85e75bb87eca585348a4173d2f0d0404fc2e1d8
zuzanna-z <131006601+zuzanna-z@users.noreply.github.com>
2025-12-03 18:35:04 +0100

    Update text_data.js

--- assets/text_data.js
+++ assets/text_data.js
@@ -7,13 +7,14 @@ const intro = 'Welcome to the experiment! If you are ready to begin, press space
 
 const instructions_trial = 'You are about to start a practice trial. Here you will be presented with a shape. Your task is to decide whether the shape on the screen is a triangle or a circle. You must press the "w" key on your keyboard, if the shape is a triangle or the "n" key  if the shape is a circle. Press "space" to begin.'
 
-const instructions_polish = 'You have finished the practice task. You will now repeat the experiment on words instead of shapes. This time you will be presented with a word and must decide whether it is a real word or a nonsense word (i.e. string of letters with no meaning behind it). You must press the "w" key on your keyboard for a real word or the "n" key for a nonsense word. Press space to begin.'
+const instructions_polish = 'You have finished the practice task. You will now repeat the experiment on words instead of shapes. This time you will be presented with a word (in Polish) and must decide whether it is a real word or a nonsense word (i.e. string of letters with no meaning behind it). You must press the "w" key on your keyboard for a real word or the "n" key for a nonsense word. Press space to begin.'
 
-const instructions_english = 'You have now finished the first test stage of this experiment. You will now repeat the same task. Once again you will be presented with a word, you must decide whether it is a real word or a nonsense word. You must press the "w" key for a real word or the "n" key for a nonsense word. Press space to begin.'
+const instructions_english = 'You have now finished the first test stage of this experiment. You will now repeat the same task. Once again you will be presented with a word (this time in English), you must decide whether it is a real word or a nonsense word. You must press the "w" key for a real word or the "n" key for a nonsense word. Press space to begin.'
 
 const instructions_control = 'This is the last stage of the experiment! This stage tests your knowledge of the English words you were presented with in the previous stage. Each word will be displayed with 4 possible translations. To answer simply click on the option of your choosing. Remember that it is NOT possible to change your answer once you check one of the boxes.'
 
 const outro = 'You have completed the experiment. I will now ask you to download and upload your data to a form linked below. The button displayed underneath will download a .csv file to your local download folder (the file name should be formated as [your_id]_data.csv). To upload the data simply open the file you just downloaded, select all and copy (Windows: press ctrl + a and then ctrl + c | Macbook: command + a and then command + c) then open the form link and paste it in the response window. Thank you for your  participation!'
 
 const end_form_instructions = 'I will now ask you to fill out a short form. The information you share will be completely anonymized and used only for the purposes of this study.'
+
 
